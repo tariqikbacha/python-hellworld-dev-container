cfe58b38c33f18eb3d579834e4ca5411b58fcef0
Tariq Jadoon <tariqv3@gmail.com>
2024-09-23 09:07:03 +0000

    function and f string

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
 print("Hello world!")
+def addTwoNums(num:int,num2:int,num3:int):
+    result:int=num+num2+num3
+    print(result)
+addTwoNums(11,22,33)
+
+name="Tariq Jadoon"
+profession="socioPolitical worker"
+country="Pakistan"
+result=f"Hello! I am {name}, a {profession} belong to {country}."
+print(result)
+addTwoNums(11,11,11)
+
